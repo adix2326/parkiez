554c711015975f751f5f922378cf22f0e8a707dc
Aditya Dhamale <aditya1dhamale1@gmail.com>
2024-03-16 18:07:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/navbar'
+
+const App = () => {
+  return (
+    <>
+    <div className='maincontainer'>
+      <Navbar/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ParkiezLogo from '../assets/images/parkiez_logo.png'
+
+
+const navbar = () => {
+  return (
+    <>
+    <div className='maincontainer'>
+      <img width={200} src={ParkiezLogo} alt="logo" />
+    </div>
+    </>
+  )
+}
+
+export default navbar
