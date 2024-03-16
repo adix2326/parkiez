5bb7377ab18b68e6b858a92422b54325f3b34adc
dev.end_ra <dhamaledevendra1@gmail.com>
2024-03-16 22:09:55 +0530

    navbar done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import ParkiezLogo from '../assets/images/parkiez_logo.png'
-
+import ParkiezLogo from '../assets/images/parkiez_logo1.png'
+import NotificationsIcons from '@mui/icons-material/Notifications'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 
 const navbar = () => {
   return (
     <>
-    <div className='maincontainer'>
-      <img width={300} src={ParkiezLogo} alt="logo" />
+    <div className='maincontainer border rounded-b-xl m-auto w-[90vw] flex flex-row no-wrap justify-between items-center bg-gray-200'>
+      <img className="ml-[25px]" width={200} src={ParkiezLogo} alt="logo" />
+      <div className="right text-gray-500 text-xl">
+        <NotificationsIcons className="mr-8" style={{ fontSize: '1.2em'}}/>
+        <AccountCircleIcon className="mr-6" style={{ fontSize: '1.8em'}}/>
+      </div>
     </div>
     </>
   )
